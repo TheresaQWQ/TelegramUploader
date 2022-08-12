16857388b652f246ec96c9e18713705177b62a0b
TheresaQWQ <yxiao2044@gmail.com>
2022-08-12 15:46:48 +0800

    优化速度显示

--- index.js
+++ index.js
@@ -69,9 +69,9 @@ const upload = async (file, target, text) => {
   const w = videoStream.width
   const h = videoStream.height
   const duration = Math.round(Number(videoStream.duration))
-  const total = fs.statSync(file).size
+  const total = fs.statSync(file).size / 1024 / 1024
 
-  const bar = new ProgressBar(`Uploading [:bar] :rate/bps :percent :etas`, {
+  const bar = new ProgressBar(`Uploading [:bar] :rateMB/s :percent :etas`, {
     total: total
   })
 
